e40dc919d68fb7e7f6c7237b037424fff381bb48
MikBV <bmv00791@gmail.com>
2024-11-14 13:25:03 +0800

    Extra commit for checking GIT work

--- src/main/java/web/config/WebConfig.java
+++ src/main/java/web/config/WebConfig.java
@@ -24,7 +24,6 @@ public class WebConfig implements WebMvcConfigurer {
         this.applicationContext = applicationContext;
     }
 
-
     @Bean
     public SpringResourceTemplateResolver templateResolver() {
         SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
